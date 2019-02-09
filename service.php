3990738df61173d984a9187bae2a95446f8b1160
Salvi Pascual <salvi.pascual@gmail.com>
2019-02-09 16:06:25 -0500

    make changes

--- service.php
+++ service.php
@@ -12,7 +12,7 @@ class Service
 	public function _main(Request $request, Response $response)
 	{
 		$response->setCache("year");
-		$response->setTemplate("home.ejs", []);
+		$response->setTemplate("home.ejs");
 	}
 
 	/**
@@ -31,19 +31,19 @@ class Service
 		// check if coupon cannot be found
 		if(empty($coupon)) {
 			return $response->setTemplate("message.ejs", [
-				"header"=>"El cupon no existe",
+				"header"=>"El cupón no existe",
 				"icon"=>"sentiment_very_dissatisfied",
-				"text" => "El cupon insertado ($couponCode) no existe o se encuentra desactivado. Por favor revise los caracteres insertados e intente nuevamente."
+				"text" => "El cupón insertado ($couponCode) no existe o se encuentra desactivado. Por favor revise su cupón e intente nuevamente."
 			]);
 		}
 
 		// check if the coupon has been used already by the user
-		$used = Connection::query("SELECT COUNT(id) AS used FROM _cupones_used WHERE email='{$request->person->email}' AND coupon='$couponCode'")[0]->used;
+		$used = Connection::query("SELECT COUNT(id) AS used FROM _cupones_used WHERE person_id='{$request->person->id}' AND coupon='$couponCode'")[0]->used;
 		if($used) {
 			return $response->setTemplate("message.ejs", [
-				"header"=>"El cupon ya fue usado",
+				"header"=>"El cupón ya fue usado",
 				"icon"=>"sentiment_very_dissatisfied",
-				"text" => "Lo sentimos, pero el cupon insertado ($couponCode) ya fue usado por usted, y solo puede aplicarse una vez por usuario."
+				"text" => "Lo sentimos, pero el cupón insertado ($couponCode) ya fue usado por usted, y solo puede aplicarse una vez por usuario."
 			]);
 		}
 
@@ -53,9 +53,9 @@ class Service
 			$cnt = Connection::query("SELECT COUNT(id) AS cnt FROM _cupones_used WHERE coupon='$couponCode'")[0]->cnt;
 			if($coupon->rule_limit <= $cnt) {
 				return $response->setTemplate("message.ejs", [
-					"header"=>"El cupon alcanzo su maximo",
+					"header"=>"El cupón alcanzo su máximo",
 					"icon"=>"sentiment_very_dissatisfied",
-					"text" => "Este cupon ($couponCode) ha sido usado demasidas veces y ahora se encuentra desactivado."
+					"text" => "Este cupón ($couponCode) ha sido usado demasidas veces y ahora se encuentra desactivado."
 				]);
 			}
 		}
@@ -65,9 +65,9 @@ class Service
 			$newUser = Connection::query("SELECT COUNT(email) AS newuser FROM person WHERE email = '{$request->person->email}' AND DATEDIFF(NOW(), insertion_date) < 3")[0]->newuser;
 			if( ! $newUser) {
 				return $response->setTemplate("message.ejs", [
-					"header"=>"El cupon no aplica",
+					"header"=>"El cupón no aplica",
 					"icon"=>"sentiment_very_dissatisfied",
-					"text" => "Lo sentimos, pero el cupon insertado ($couponCode) solo puede aplicarse a nuevos usuarios."
+					"text" => "Lo sentimos, pero el cupón insertado ($couponCode) solo puede aplicarse a nuevos usuarios."
 				]);
 			}
 		}
@@ -76,9 +76,9 @@ class Service
 		if($coupon->rule_deadline) {
 			if(date('Y-m-d') > date('Y-m-d', strtotime($coupon->rule_deadline))) {
 				return $response->setTemplate("message.ejs", [
-					"header"=>"El cupon ha expirado",
+					"header"=>"El cupón ha expirado",
 					"icon"=>"sentiment_very_dissatisfied",
-					"text" => "Lo sentimos, pero el cupon insertado ($couponCode) ha expirado y no puede ser usado."
+					"text" => "Lo sentimos, pero el cupón insertado ($couponCode) ha expirado y no puede ser usado."
 				]);
 			}
 		}
@@ -88,13 +88,13 @@ class Service
 		Connection::query("UPDATE person SET credit=credit+$credits WHERE email='{$request->person->email}'");
 
 		// create records of your interaction
-		Connection::query("INSERT INTO _cupones_used(coupon, email) VALUES ('$couponCode', '{$request->person->email}')");
+		Connection::query("INSERT INTO _cupones_used(coupon, person_id) VALUES ('$couponCode', '{$request->person->id}')");
 
 		// offer rewards response
 		$response->setTemplate("message.ejs", [
-			"header"=>"&iexcl;Felicidades!",
+			"header"=>"¡Felicidades!",
 			"icon"=>"sentiment_very_satisfied",
-			"text" => "Su cupon se ha canjeado correctamente y usted ha ganado <b>&sect;$credits en creditos de Apretaste</b>. Gracias por canjear su cupon."
+			"text" => "Su cupón se ha canjeado correctamente y usted ha ganado §$credits en créditos de Apretaste. Gracias por canjear su cupón."
 		]);
 	}
 }
