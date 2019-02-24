16aae229e4b3e77c21b57bc9cf4f8887140a06b6
Salvi Pascual <salvi.pascual@gmail.com>
2019-02-24 11:23:04 -0500

    fix coupon

--- scripts.js
+++ scripts.js
@@ -1,19 +1,16 @@
-var service = {
-	// get the value from the 
-	submit: function() {
-		// get the coupon
-		var coupon = $('#coupon').val();
+function sendCoupon() {
+	// get the coupon
+	var coupon = $('#coupon').val();
 
-		// check the coupon is not empty
-		if( ! coupon) {
-			M.toast({html: 'El cup&oacute;n no puede estar vac&iacute;o'});
-			return false;
-		}
-
-		// send the request
-		apretaste.send({
-			command: "CUPONES CANJEAR", 
-			data: {"coupon":coupon},
-			redirect: true});
+	// check the coupon is not empty
+	if( ! coupon) {
+		M.toast({html: 'El cupón no puede estar vacío'});
+		return false;
 	}
+
+	// send the request
+	apretaste.send({
+		command: "CUPONES CANJEAR", 
+		data: {"coupon": coupon},
+		redirect: true});
 }
